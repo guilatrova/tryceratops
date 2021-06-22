df884cb1017d70f23d2d66d71c248e8f622f6f75
Guilherme Latrova <hello@guilatrova.dev>
2021-06-22 06:52:59 -0300

    tests: add new scenario

---
+++ src/tests/samples/violations/try_consider_else.py
@@ -0,0 +1,32 @@
+"""
+Violation:
+
+Returning a final value inside a try block may indicate you could use an else block
+instead to outline the success scenario
+"""
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class MyException(Exception):
+    pass
+
+
+def good():
+    try:
+        a = 1
+        b = process()
+        return b
+    except MyException:
+        logger.exception("process failed")
+
+
+def good():
+    try:
+        a = 1
+        b = process()
+    except MyException:
+        logger.exception("process failed")
+    else:
+        return b
