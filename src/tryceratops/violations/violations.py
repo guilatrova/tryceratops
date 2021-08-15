c0639c0d35baeeee171c96b5f1448a2cf15ef53d
Guilherme Latrova <hello@guilatrova.dev>
2021-08-15 15:11:14 -0300

    refactor: create violation subtype

--- src/tryceratops/violations/violations.py
+++ src/tryceratops/violations/violations.py
@@ -1,6 +1,6 @@
 import ast
 from dataclasses import dataclass
-from typing import Tuple
+from typing import Optional, Tuple
 
 
 @dataclass
@@ -10,8 +10,26 @@ class Violation:
     col: int
     description: str
     filename: str
+    node: Optional[ast.AST] = None
 
     @classmethod
-    def build(cls, filename: str, vio_details: Tuple[str, str], stmt: ast.stmt):
+    def build(cls, filename: str, vio_details: Tuple[str, str], node: ast.AST, **kwargs):
         code, msg = vio_details
-        return cls(code, stmt.lineno, stmt.col_offset, msg, filename)
+        return cls(code, node.lineno, node.col_offset, msg, filename, node)
+
+
+@dataclass
+class VerboseReraiseViolation(Violation):
+    exception_name: str = ""
+
+    @classmethod
+    def build(
+        cls,
+        filename: str,
+        vio_details: Tuple[str, str],
+        node: ast.AST,
+        exception_name: str = "",
+        **kwargs
+    ):
+        code, msg = vio_details
+        return cls(code, node.lineno, node.col_offset, msg, filename, node, exception_name)
